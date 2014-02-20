f37f901e0011e632bdd03905c083358e3350506d
xing240 <787923046@qq.com>
2014-02-20 12:02:59 +0800

    perfect index

--- js/indexJs.js
+++ js/indexJs.js
@@ -1,46 +1,48 @@
 // 移动main中的内容
+var memory = "0";
+var move = 0;
 
 function scroll(e) {
-	for (var i = 0; i < 3; i++) {
-		document.getElementById('page' + i).style.color = "rgb(236, 199, 9)";
-		document.getElementById('page' + i).style.cursor = "pointer";
-	}
-	document.getElementById('page' + e).style.color = "rgb(238, 233, 251)";
-	document.getElementById('page' + e).style.cursor = "text";
-	var p = document.getElementById('main');
-	if (p.scrollLeft <= 0 && e != 0) {
-		var move = setInterval(function() {
-			p.scrollLeft += (6 + 4 * (e - 1));
-			if (p.scrollLeft >= 660 * e) {
-				clearInterval(move);
-				//alert(p.scrollLeft);
-			}
-		}, 1);
-	} else if (p.scrollLeft <= 700 && e != 1) {
-		if (e == 2) {
-			var move = setInterval(function() {
-				p.scrollLeft += 6;
-				if (p.scrollLeft >= 1320) {
+	if (memory != e) {
+		clearInterval(move);
+		var oldPage = document.getElementById('page' + memory);
+		oldPage.style.color = "rgb(236, 199, 9)";
+		oldPage.style.cursor = "pointer";
+		memory = e;
+		var newPage = document.getElementById('page' + e);
+		newPage.style.color = "rgb(238, 233, 251)";
+		newPage.style.cursor = "text";
+		var p = document.getElementById('main');
+		if (p.scrollLeft <= 660 && e != 0) {
+			move = setInterval(function() {
+				p.scrollLeft += (20 + 10 * (e - 1));
+				if (p.scrollLeft == 660 * e) {
 					clearInterval(move);
-					//alert(p.scrollLeft);
 				}
-			}, 1);
-		} else if (e == 0) {
-			var move = setInterval(function() {
-				p.scrollLeft -= 6;
-				if (p.scrollLeft <= 0) {
+			}, 4);
+		} else if (p.scrollLeft < 1320) {
+			if (e === 2) {
+				move = setInterval(function() {
+					p.scrollLeft += 20;
+					if (p.scrollLeft == 1320) {
+						clearInterval(move);
+					}
+				}, 4);
+			} else {
+				move = setInterval(function() {
+					p.scrollLeft -= 20;
+					if (p.scrollLeft == 660 * e) {
+						clearInterval(move);
+					}
+				}, 4);
+			};
+		} else if (p.scrollLeft >= 1320) {
+			move = setInterval(function() {
+				p.scrollLeft -= (30 - 10 * e);
+				if (p.scrollLeft == 660 * e) {
 					clearInterval(move);
-					//alert(p.scrollLeft);
 				}
-			}, 1);
-		};
-	} else if (p.scrollLeft >= 1300 && e != 2) {
-		var move = setInterval(function() {
-			p.scrollLeft -= (10 - 4 * e);
-			if (p.scrollLeft <= 660 * e) {
-				clearInterval(move);
-				//alert(p.scrollLeft);
-			}
-		}, 1);
+			}, 4);
+		}
 	}
-}
+};
